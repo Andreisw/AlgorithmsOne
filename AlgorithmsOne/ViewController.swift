99a836e9ae4649d361b93e84a3b23396c59ea35a
Andrei Cojocaru <63311574+Andreisw@users.noreply.github.com>
2021-10-29 17:50:57 +0300

    Add files via upload
    Algoritmi partea II cu testare. Fara exercitiul 7

---
+++ AlgorithmsOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlgorithmsOne
+//
+//  Created by andrei.cojocaru on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let test = AddOnesNumber()
+    let divid = Divisors()
+    let digit = CountGivenDigit()
+    let halfSum = ListHalfSum()
+    let powIterative = PowIterative()
+    let biggetsNumber = FindBiggestNumber()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+}
