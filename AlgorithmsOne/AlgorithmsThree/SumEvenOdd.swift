ade7f106aed70d182469752eb263e802919cb9f0
andrei.cojocaru <andrei.cojocaru@softvision.com>
2021-11-03 16:48:12 +0200

    feat: implement sumForFirstEvLastOd function from AlgorithmsThree to calculate the sum of first even number and last odd number from an array

--- AlgorithmsOne/AlgorithmsThree/SumEvenOdd.swift
+++ AlgorithmsOne/AlgorithmsThree/SumEvenOdd.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 class SumEvenOdd {
-    
     func sumForFirstEvLastOd(numberList: [Int]) -> Int {
-        let sum = 0
-        
-        return sum
+        var evenNumber: [Int] = []
+        var oddNumber: [Int] = []
+        if numberList == [] { return 0}
+        // Add item inside even number and odd number lists
+        for digit in numberList {
+            digit % 2 == 0 ? evenNumber.append(digit) : oddNumber.append(digit)
+        }
+        // Check if evenNumber list or oddNumber list are empty
+        if evenNumber == [] { return oddNumber[oddNumber.count - 1]}
+        if oddNumber == [] { return evenNumber[evenNumber.count - 1]}
+        return evenNumber[0] + oddNumber[oddNumber.count - 1]
     }
 }
