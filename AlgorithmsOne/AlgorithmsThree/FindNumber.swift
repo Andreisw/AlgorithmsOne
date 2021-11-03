47bce9bcf044ecc3952c40fcd75714711ee1793d
andrei.cojocaru <andrei.cojocaru@softvision.com>
2021-11-03 13:23:55 +0200

    Add .gitigonre file

---
+++ AlgorithmsOne/AlgorithmsThree/FindNumber.swift
@@ -0,0 +1,16 @@
+//
+//  FindNumber.swift
+//  AlgorithmsOne
+//
+//  Created by andrei.cojocaru on 11/3/21.
+//
+
+import Foundation
+
+class FindNumber {
+    func findIfNumberExistInList(number: Int, listNumbers: [Int]) -> Bool {
+        
+        return true
+    }
+}
+
